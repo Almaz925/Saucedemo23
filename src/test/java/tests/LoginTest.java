c7154cda8524850b710b0c50b0e946a08e6b1d8b
Lenovo <880almaz@gmail.com>
2024-06-23 21:41:28 +0400

    LoginTest added

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class LoginTest extends BaseTest {
+    @Test
+    public void correctLogin() {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+        assertEquals(driver.findElement(By.xpath("//span[@class='title']")).getText(), "Products");
+    }
+}
