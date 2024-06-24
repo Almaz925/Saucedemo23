44a4ecf8ed3b5c0aa7dd2f9042879fe057f0f907
Lenovo <880almaz@gmail.com>
2024-06-24 10:30:13 +0400

    LoginTest added

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,9 +2,11 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.assertEquals;
 
 public class LoginTest extends BaseTest {
+
     @Test
     public void correctLogin() {
         loginPage.open();
@@ -13,24 +15,24 @@ public class LoginTest extends BaseTest {
     }
 
     @Test
-    public void emptyLogin(){
+    public void emptyLogin() {
         loginPage.open();
         loginPage.login("", "secret_sauce");
-        assertEquals(driver.findElement(By.xpath("//h3[@data-test='error']")).getText(),"Epic sadface: Username is required");
+        assertEquals(driver.findElement(By.xpath("//h3[@data-test='error']")).getText(), "Epic sadface: Username is required");
 
     }
 
     @Test
-    public void emptyPassword(){
+    public void emptyPassword() {
         loginPage.open();
-        loginPage.login("standard_user","");
-assertEquals(driver.findElement(By.xpath("//h3[@data-test='error']")).getText(),"Epic sadface: Password is required" );
+        loginPage.login("standard_user", "");
+        assertEquals(driver.findElement(By.xpath("//h3[@data-test='error']")).getText(), "Epic sadface: Password is required");
     }
 
     @Test
-    public void lockedUser(){
+    public void lockedUser() {
         loginPage.open();
         loginPage.login("locked_out_user", "secret_sauce");
-        assertEquals(driver.findElement(By.xpath("//h3[@data-test='error']")).getText(),"Epic sadface: Sorry, this user has been locked out." );
+        assertEquals(driver.findElement(By.xpath("//h3[@data-test='error']")).getText(), "Epic sadface: Sorry, this user has been locked out.");
     }
 }
